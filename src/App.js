2112af32aa164fa99aeaaccff65a855ed71c3a5c
Ishan <ishanliou@yahoo.com>
2018-07-22 13:26:08 -0700

    navbar and intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import NavBar from './component/NavBar'
+import Intro from './component/Intro'
+import axios from 'axios'
 import './App.css'
 
 class App extends Component {
+  state ={
+    breedList: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://dog.ceo/api/breeds/list/all`)
+    .then(res => {
+      console.log('all breed', res.data.message)
+      this.setState({
+        breedList: Object.keys(res.data.message) // get all breeds list
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <NavBar breedList={this.state.breedList}/>
+        <Intro />
       </div>
     );
   }
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 class NavBar extends Component {
     render() {
+        const breedList = this.props.breedList
         return(
             <div className="nav-bar">
-                
+                <div className="breed-list">
+                    <select className="select-breed-list">
+                        <option>Select one</option>
+                        {breedList.map((li) => (
+                        <option key={li} value={li}>{li}</option>
+                        ))}
+                    </select>
+                </div>
             </div>
         )
 
