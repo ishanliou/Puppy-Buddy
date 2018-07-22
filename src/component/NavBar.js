3e7ad336867f7245cd1e6348cc58621376af5844
Ishan <ishanliou@yahoo.com>
2018-07-22 12:57:55 -0700

    first commit

---
+++ src/component/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+    render() {
+        return(
+            <div className="nav-bar">
+                
+            </div>
+        )
+
+    }
+}
+
+export default NavBar
