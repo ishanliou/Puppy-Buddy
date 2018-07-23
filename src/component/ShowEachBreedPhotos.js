bdd7ff0be37f66df88aebcaf43f601dc4a93ddd5
Ishan <ishanliou@yahoo.com>
2018-07-22 22:56:25 -0700

    display selected breed box

--- src/component/ShowEachBreedPhotos.js
+++ src/component/ShowEachBreedPhotos.js
@@ -11,22 +11,37 @@ class ShowEachBreedPhotos extends Component {
         console.log('component did mount in show each breed photos')
         const breedPhotosURL = `https://dog.ceo/api/breed/${this.props.showBreed}/images`
         axios.get(breedPhotosURL)
-        .then(res => {
-            console.log("photos",res.data.message)
-            this.setState({
-                photo_list: res.data.message
+            .then(res => {
+                console.log("photos",res.data.message)
+                this.setState({
+                    photo_list: res.data.message
+                })
             })
-        })
     }
 
     render() {
-        
         return(
             <div>
-                Show:{this.props.showBreed}
-                Photo: {this.state.photo_list.map( (i, id) =>
+                <div className="gds-flex-grid__container">
+                    <div className="gds-flex-grid__row">
+                        {this.state.photo_list.map( (i, index) => (
+                            <div className="gds-flex-grid__item gds-flex-grid__item--desktop-4 gds-flex-grid__item--tablet-2 gds-flex-grid__item--mobile-1 -m-b-3">
+                                <div className="gds-card gds-flex-grid__item--full-height">
+                                    <div className="gds-card__img-container">
+                                        <img className="gds-card__img" src={i} key={index}/>
+                                        <div className="gds-card__img-helper"></div>
+                                    </div>
+                                </div>
+                            </div>   
+                        ))}
+                             
+                    </div>
+                </div>
+            
+
+                {/* Photo: {this.state.photo_list.map( (i, id) =>
                     <img src={i} key={id}/>
-                )}
+                )} */}
             </div>
         )
     }
