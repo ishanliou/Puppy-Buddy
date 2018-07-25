bdbd2b211088b414b5a4345eb44a42478b947dd7
Ishan <ishanliou@yahoo.com>
2018-07-25 16:23:19 -0700

    sub breed page

--- src/component/ShowSubBreedPhotos.js
+++ src/component/ShowSubBreedPhotos.js
@@ -25,25 +25,43 @@ class ShowSubBreedPhotos extends Component {
 
     render() {
       const { selectedBreedPhotoList } = this.state
-      const  showSubBreed  =`${this.props.getSelectedBreed}-${this.props.showSubBreed}`
+      const  selectedSubBreed  =`${this.props.getSelectedBreed}-${this.props.showSubBreed}`
       const filtered = selectedBreedPhotoList.filter(e =>
-        e.includes(showSubBreed))
+        e.includes(selectedSubBreed))
       console.log('filtered', filtered)
       // console.log('showSubBreed', showSubBreed)
       // console.log('selectedBreedPhotoList', selectedBreedPhotoList)
-        return(
-          
+        return(  
             <div>
-              sub:
-              {showSubBreed}
-
-              photo: 
-              
-              <div>
-                {filtered.length>0 ? filtered.map((photo) => (
-                  <img src={photo} />
-                )): <div>No Subbreed...</div>} 
+              <div className="show-breed-name gds-layout__container">
+                <h1 className="show-breed-header gds-text--header-lg -m-b-3">
+                  {this.capitalizeFirstLetter(this.props.getSelectedBreed)} 
+                </h1>
               </div>
+              <div>
+                <h4 class="gds-text--body-md -m-b-5"> 
+                  Sub-breed: {this.props.showSubBreed}
+                </h4>
+              </div>
+              <div className="-z-0">
+                    <div className="gds-flex-grid__container gds-layout__container">
+                        <div className="gds-flex-grid__row">
+                            {filtered.length > 0 ? filtered.map((photo, index) => (
+                                <div className="gds-flex-grid__item gds-flex-grid__item--desktop-4 gds-flex-grid__item--tablet-2 gds-flex-grid__item--mobile-1 -m-b-3" key={index}>
+                                    <div className="gds-card-no-bottom gds-card gds-flex-grid__item--full-height">
+                                        <div className="gds-card__img-container">
+                                            <img className="gds-card__img" src={photo}  alt={this.props.showSubBreed}/>
+                                            <div className="gds-card__img-helper"></div>
+                                        </div>
+                                    </div>
+                                </div>   
+                            )): <div class="gds-loading">
+                                  <div class="gds-loading__dot"></div>
+                                </div>
+                            }                                
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
