25d489f58131091f88fb88cf914ae76d736b49b6
Ishan <ishanliou@yahoo.com>
2018-07-26 01:15:41 -0700

    styling

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -11,24 +11,24 @@ const Footer = () => {
         <img src={logo} style={{width:"36px"}} alt="logo"/>          
       </div>
       <div className="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-r-2 -p-t-1">
-        <img src={`fb_logo.svg`} style={{width:"30px"}}/>
+        <img src={`fb_logo.svg`} style={{width:"30px"}} alt="fb-logo"/>
       </div>
       <div className="decorate decorate--all gds-flex__item gds-flex__item--grow-0">
-        <img src={`IG_logo.png`} style={{width:"40px"}}/>
+        <img src={`IG_logo.png`} style={{width:"40px"}} alt="IG-logo"/>
       </div>
       <div className="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-r-1">
-        <img src={`Twitter_logo.svg`} style={{width:"36px"}}/>
+        <img src={`Twitter_logo.svg`} style={{width:"36px"}} alt="twitter-logo"/>
       </div>
-      <div class="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-t-2"><span class="gds-badge gds-badge--empty"></span></div>
-      <div class="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-t-2"><span class="gds-badge gds-badge--empty gds-badge--success"></span></div>
-      <div class="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-t-2"><span class="gds-badge gds-badge--empty gds-badge--warning"></span></div>
-      <div class="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-t-2"><span class="gds-badge gds-badge--empty gds-badge--danger"></span></div>
-      <div class="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-t-2"><span class="gds-badge gds-badge--empty gds-badge--info"></span></div>
+      <div className="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-t-2"><span className="gds-badge gds-badge--empty"></span></div>
+      <div className="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-t-2"><span className="gds-badge gds-badge--empty gds-badge--success"></span></div>
+      <div className="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-t-2"><span className="gds-badge gds-badge--empty gds-badge--warning"></span></div>
+      <div className="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-t-2"><span className="gds-badge gds-badge--empty gds-badge--danger"></span></div>
+      <div className="decorate decorate--all gds-flex__item gds-flex__item--grow-0 -m-t-2"><span className="gds-badge gds-badge--empty gds-badge--info"></span></div>
 
     </div>
 
 
-<div class="gds-flex gds-flex--justify-end">
+<div className="gds-flex gds-flex--justify-end">
 </div>
 
 
