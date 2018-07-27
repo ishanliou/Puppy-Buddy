85052634a6f12a587dfe992c3fab0ec15ffdd111
Ishan <ishanliou@yahoo.com>
2018-07-26 23:48:09 -0700

    intro center

--- src/component/Intro.js
+++ src/component/Intro.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Intro = () => {
   return (
-    <div id="intro" className="gds-layout__container">
-      <div className="intro-img gds-image -m-b-2">
+    <div id="intro" className="gds-layout__container gds-flex gds-flex--direction-col gds-flex--align-center">
+      <div className="intro-img gds-image -m-b-2 ">
           <img src={`puppybuddy.png`} alt='intro'style={{width:"300px", heigh:"auto"}} />
       </div>
       <div>
-        <p className="gds-text--body-md gds-layout__column--md-6 gds-layout__column--xs-offset-3">
+        <p className="gds-text--body-md decorate decorate--all gds-flex__item gds-flex__item--grow-0">
           <span className="gds-text--bold">Puppy Buddy</span> is a photo collection of 86 dog breeds
           that you can browse on the website. Also, if there is any sub breeds, you can filter the photos by sub breed. 
         </p> 
